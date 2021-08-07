9c17337c0988acf24fa74d7b66f635a7b1233d5c
Shashank-H <shashank.h@juegostudio.net>
2021-08-07 11:36:47 +0530

    Router setup and styling changes

--- quotes-app/src/App.js
+++ quotes-app/src/App.js
@@ -1,12 +1,24 @@
-import { Home } from './pages/Home/Home'
-import './App.css';
 
+import './App.css';
+import { Home } from './pages/Home/Home';
+import { CategoryQuote } from './pages/CategoryQuote/CategoryQuote';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home" component={Home}/>
+        <Route path="/quote/:category" component={CategoryQuote} />
+        <Redirect exact path="/" to="/home" />
+      </Switch>
+    </Router>
   );
 }
 
