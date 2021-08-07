67aff1b0e34712cf567ace51c9f02990bbf37dcd
Shashank-H <shashank.h@juegostudio.net>
2021-08-07 11:40:21 +0530

    Heder Implementation

--- quotes-app/src/components/Header.js
+++ quotes-app/src/components/Header.js
@@ -1,12 +1,11 @@
+import './Header.css'
 
-import './Header.css';
-
-export function Header(){
-
+export function Header({ ...props }) {
   return (
     <div className="header">
-      <img src="https://media2.giphy.com/media/2A4A9kI7YFXC789O7A/giphy.gif" className="logo"/>
-      <h2 className="app-name" >Quotes App</h2>
+      <img src="https://media1.giphy.com/media/2A4A9kI7YFXC789O7A/giphy.gif" className="header__logo" />
+
+      <h1>Quotes App</h1>
     </div>
-  )
+  );
 }
